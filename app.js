11baa03c9f1191b27dd9550ec0220604880d1945
Kamil Chec <k.chec.prv@gmail.com>
2022-09-16 15:14:36 +0200

    Add active menu scrolling animation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
 
 //Display mobile menu
 const mobileMenu = () => {
@@ -8,3 +9,43 @@ const mobileMenu = () => {
 };
 
 menu.addEventListener('click', mobileMenu);
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+  const element = document.querySelector('.highlight');
+  const homeMenu = document.querySelector('#home-page');
+  const aboutMenu = document.querySelector('#about-page');
+  const services = document.querySelector('#services-page');
+
+  let scrollPosition = window.scrollY;
+
+  if (window.innerWidth < 960) {
+    return;
+  }
+
+  if (scrollPosition < 600) {
+    homeMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  }
+
+  if (scrollPosition < 1400) {
+    aboutMenu.classList.add('highlight');
+    homeMenu.classList.remove('highlight');
+    services.classList.remove('highlight');
+    return;
+  }
+
+  if (scrollPosition < 2345) {
+    services.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  }
+
+  if ((element && scrollPosition < 600) || element) {
+    element.classList.remove('highlight');
+  }
+};
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
